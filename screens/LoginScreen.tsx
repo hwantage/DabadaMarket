30166b97137b70df0e94f50f0d16ac3263878b86
DESKTOP-Q9CRHL0\hwant <hwan77@somansa.com>
2022-10-17 00:06:30 +0900

    내 정보 수정 기능 구현
    상품 등록 기능 구현 중

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,14 +1,15 @@
 import React, {useState, useRef} from 'react';
 import {ActivityIndicator, Alert, Keyboard, KeyboardAvoidingView, Platform, StyleSheet, View, TextInput, Image} from 'react-native';
 import {default as Text} from '../components/common/DabadaText';
-import BorderedInput from '../components/common/BorderedInput';
+import DabadaInput from '../components/common/DabadaInput';
 import DabadaButton from '../components/common/DabadaButton';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useNavigation, useRoute, RouteProp} from '@react-navigation/native';
 import type {StackNavigationProp} from '@react-navigation/stack';
 import {useSetRecoilState} from 'recoil';
 import {authInfoProps, authInfoState} from '../recoil/authInfoAtom';
-import {signIn, join, getUserInfo} from '../utils/auth';
+import {login, join, getUserInfo} from '../utils/auth';
+import {useTranslation} from 'react-i18next';
 
 type ParamList = {
   params: {
@@ -18,6 +19,7 @@ type ParamList = {
 
 function LoginScreen() {
   const navigation = useNavigation<StackNavigationProp<any>>();
+  const {t} = useTranslation();
   const route = useRoute<RouteProp<ParamList, 'params'>>();
   const ref_password = useRef<TextInput>(null);
   const ref_confirmPassword = useRef<TextInput>(null);
@@ -40,24 +42,24 @@ function LoginScreen() {
     Keyboard.dismiss();
 
     if (form.email.trim() === '') {
-      Alert.alert('실패', '이메일을 입력하십시오.');
+      Alert.alert(t('common.fail', '실패'), t('msg.enterEmail', '이메일을 입력하십시오.'));
       return;
     }
 
     if (form.password.trim() === '') {
-      Alert.alert('실패', '비밀번호를 입력하십시오.');
+      Alert.alert(t('common.fail', '실패'), t('msg.enterPassword', '비밀번호를 입력하십시오.'));
       return;
     }
 
     if (isJoin && form.password !== form.confirmPassword) {
-      Alert.alert('실패', '비밀번호가 일치하지 않습니다.');
+      Alert.alert(t('common.fail', '실패'), t('msg.notMatchPassword', '비밀번호가 일치하지 않습니다.'));
       return;
     }
 
     setLoading(true);
 
     try {
-      const {user} = isJoin ? await join(form.email, form.password) : await signIn(form.email, form.password);
+      const {user} = isJoin ? await join(form.email, form.password) : await login(form.email, form.password);
 
       console.log('user :: ', user);
 
@@ -65,24 +67,25 @@ function LoginScreen() {
       console.log('PROFILE :', userInfo);
 
       if (!userInfo) {
-        setAuthInfo({id: user.uid, displayName: '', photoURL: ''});
+        // 프로필 설정한 적 없으면 프로필 수정 화면으로 이동.
+        setAuthInfo({u_id: user.uid, u_nickname: '', u_group: '소만사', u_photoUrl: '', u_lang: 'ko'});
         navigation.navigate('MyProfileModifyScreen');
       } else {
         setAuthInfo(userInfo);
       }
     } catch (e: any) {
       if (e.code === 'auth/email-already-in-use') {
-        Alert.alert('실패', '이미 가입된 이메일입니다.');
+        Alert.alert(t('common.fail', '실패'), t('msg.emailAlreadyInUse', '이미 가입된 이메일입니다.'));
       } else if (e.code === 'auth/wrong-password') {
-        Alert.alert('실패', '잘못된 비밀번호입니다.');
+        Alert.alert(t('common.fail', '실패'), t('msg.wrongPassword', '잘못된 비밀번호입니다.'));
       } else if (e.code === 'auth/user-not-found') {
-        Alert.alert('실패', '존재하지 않는 계정입니다.');
+        Alert.alert(t('common.fail', '실패'), t('msg.userNotFound', '존재하지 않는 계정입니다.'));
       } else if (e.code === 'auth/invalid-email') {
-        Alert.alert('실패', '유효하지 않은 이메일 주소입니다.');
+        Alert.alert(t('common.fail', '실패'), t('msg.invalidEmail', '유효하지 않은 이메일 주소입니다.'));
       } else if (e.code === 'auth/weak-password') {
-        Alert.alert('실패', '비밀번호는 6자리 이상입니다.');
+        Alert.alert(t('common.fail', '실패'), t('msg.weakPassword', '비밀번호는 6자리 이상입니다.'));
       } else {
-        Alert.alert('실패', '실패했습니다.');
+        Alert.alert(t('common.fail', '실패'), t('msg.fail', '실패했습니다.'));
       }
       console.log(e);
     } finally {
@@ -93,12 +96,12 @@ function LoginScreen() {
   return (
     <KeyboardAvoidingView style={styles.keyboardAvoidingView} behavior={Platform.select({ios: 'padding'})}>
       <SafeAreaView style={styles.fullscreen}>
-        <Text style={styles.text}>Dabada Market</Text>
+        <Text style={styles.text}>{t('title', '다바다 마켓')}</Text>
         <Image style={styles.circle} source={require('../assets/dabada.png')} />
         <View style={styles.form}>
-          <BorderedInput hasMarginBottom={true} placeholder="이메일" value={form.email} autoCapitalize="none" autoCorrect={false} autoCompleteType="email" keyboardType="email-address" returnKeyType="next" blurOnSubmit={false} onChangeText={(text: string) => createChangeTextHandler('email', text)} onSubmitEditing={() => ref_password.current?.focus()} />
-          <BorderedInput hasMarginBottom={true} placeholder="비밀번호" value={form.password} secureTextEntry blurOnSubmit={false} returnKeyType={isJoin ? 'next' : 'done'} onChangeText={(text: string) => createChangeTextHandler('password', text)} onSubmitEditing={() => (isJoin ? ref_confirmPassword.current?.focus() : onSubmit())} ref={ref_password} />
-          {isJoin && <BorderedInput hasMarginBottom={false} placeholder="비밀번호 확인" value={form.confirmPassword} secureTextEntry blurOnSubmit={false} returnKeyType="done" onChangeText={(text: string) => createChangeTextHandler('confirmPassword', text)} onSubmitEditing={onSubmit} ref={ref_confirmPassword} />}
+          <DabadaInput hasMarginBottom={true} placeholder={t('common.email', '이메일')} value={form.email} autoCapitalize="none" autoCorrect={false} autoCompleteType="email" keyboardType="email-address" returnKeyType="next" blurOnSubmit={false} onChangeText={(text: string) => createChangeTextHandler('email', text)} onSubmitEditing={() => ref_password.current?.focus()} />
+          <DabadaInput hasMarginBottom={true} placeholder={t('common.password', '비밀번호')} value={form.password} secureTextEntry blurOnSubmit={false} returnKeyType={isJoin ? 'next' : 'done'} onChangeText={(text: string) => createChangeTextHandler('password', text)} onSubmitEditing={() => (isJoin ? ref_confirmPassword.current?.focus() : onSubmit())} ref={ref_password} />
+          {isJoin && <DabadaInput hasMarginBottom={false} placeholder={t('common.passwordConfirm', '비밀번호 확인')} value={form.confirmPassword} secureTextEntry blurOnSubmit={false} returnKeyType="done" onChangeText={(text: string) => createChangeTextHandler('confirmPassword', text)} onSubmitEditing={onSubmit} ref={ref_confirmPassword} />}
           {loading && (
             <View style={styles.spinnerWrapper}>
               <ActivityIndicator size={32} color="#6200ee" />
@@ -108,9 +111,9 @@ function LoginScreen() {
             <View style={styles.buttons}>
               {!isJoin && (
                 <>
-                  <DabadaButton title="로그인" hasMarginBottom={true} onPress={onSubmit} />
+                  <DabadaButton title={t('common.login', '로그인')} hasMarginBottom={true} onPress={onSubmit} />
                   <DabadaButton
-                    title="회원가입"
+                    title={t('common.join', '회원가입')}
                     theme="secondary"
                     hasMarginBottom={false}
                     onPress={() => {
@@ -121,9 +124,9 @@ function LoginScreen() {
               )}
               {isJoin && (
                 <>
-                  <DabadaButton title="회원가입" hasMarginBottom={true} onPress={onSubmit} />
+                  <DabadaButton title={t('common.join', '회원가입')} hasMarginBottom={true} onPress={onSubmit} />
                   <DabadaButton
-                    title="로그인"
+                    title={t('common.login', '로그인')}
                     theme="secondary"
                     hasMarginBottom={false}
                     onPress={() => {
